c40654c49722c38e2247612f1e7a62cc06d4aad8
oliullahakib <oliullahakib@gmail.com>
2025-09-20 18:03:56 +0600

    edit functionaliy and localStorage added but net to finx a bug of localStorage

--- src/componets/Main/Main.jsx
+++ src/componets/Main/Main.jsx
@@ -1,13 +1,26 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import Todo from '../Todo/Todo'
 
 const Main = () => {
     const [todo, setTodo] = useState("")
     const [todos, setTodos] = useState([])
+    useEffect(() => {
+      let todosString = JSON.parse(localStorage.getItem('todos'));
+     if(todosString){
+        setTodos(todosString)
+     }
+    }, [])
+    
+    const setToLS=()=>{
+        console.log(todos)
+        let todosJS =JSON.stringify(todos) 
+        localStorage.setItem("todos",todosJS)
+    }
     const handleSave =()=>{
-        todo && setTodos([...todos,todo])
-         setTodo('')
-        
+        todo &&  setTodos([...todos,todo])
+        console.log(todos)
+        setTodo('')
+        setToLS()
     }
     const handelChange =(e)=>{
         setTodo(e.target.value)
@@ -26,7 +39,7 @@ const Main = () => {
                     <h1 className='text-2xl font-bold'>Your Todos</h1>
                 </div>
                {
-                todos.map((item,index)=><Todo key={index} item ={item} id={index} todos={[todos,setTodos]}/>)
+                todos.map((item,index)=><Todo key={index} item ={item} id={index} todo={[todo,setTodo]} todos={[todos,setTodos]} setToLS={setToLS} />)
                }
             </div>
         </div>
--- src/componets/Todo/Todo.jsx
+++ src/componets/Todo/Todo.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 
-const Todo = ({item,id,todos}) => {
+const Todo = ({item,id,todos,todo,setToLS}) => {
     const handleDelete =(id)=>{
         let filterTodos = todos[0].filter((_,index)=>index!==id)
         todos[1](filterTodos);
+        
+    }
+    const handleEdit =(id)=>{
+        // set value 
+        let findTodo = todos[0].find((_,index)=>index===id);
+        todo[1](findTodo)
+        // delete value
+        let filterTodos = todos[0].filter((_,index)=>index!==id)
+        todos[1](filterTodos);
+        setToLS()
     }
     return (
         <div className='todo flex justify-between w-1/4 items-center'>
-            <div id={id} className='flex'>
+            <div id={id} className='flex items-center gap-1'>
                 <input type="checkbox" className="checkbox checkbox-sm bg-black text-white" />
                 <p>{item}</p>
             </div>
             <div className='flex'>
-                <button className='btn btn-primary'>Edit</button>
+                <button onClick={()=>{handleEdit(id)}} className='btn btn-primary'>Edit</button>
                 <button onClick={()=>{handleDelete(id)}} className='btn btn-primary ml-3 '>Delete</button>
             </div>
         </div>
